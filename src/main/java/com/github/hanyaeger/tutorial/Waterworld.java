941a7f7d1f6771a0aeb81e887dc8d43411d9031a
EmielBertrand <emielbertrand@gmail.com>
2022-10-18 15:17:13 +0200

    Create Waterworld.java

---
+++ src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
@@ -0,0 +1,18 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class Waterworld extends YaegerGame {
+    public static void main(String[] args){
+
+        launch(args);
+    }
+    @Override
+    public void setupGame(){
+
+    }
+    @Override
+    public void setupScenes() {
+
+    }
+}
